52c27316cbe4c73e6ef2c27239cac3e819696580
dwayhs <daniel.wayhs@toptal.com>
2020-03-22 10:56:58 -0300

    Set dayWidth to 22

--- app/App.js
+++ app/App.js
@@ -29,7 +29,7 @@ module.exports = class App {
     const target = document.getElementById('render-target')
     new Render(gantt, {
       elementHeight: 20,
-      dayWidth: 28,
+      dayWidth: 22,
     }).render(target)
   }
 }
