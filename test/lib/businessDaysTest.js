a26ee31b23956c2e679e91474548e8d1c6398d03
dwayhs <daniel.wayhs@toptal.com>
2020-03-26 11:18:40 -0300

    Test addBusinessDays

--- test/lib/businessDaysTest.js
+++ test/lib/businessDaysTest.js
@@ -1,5 +1,6 @@
-const { assert } = require('chai')
+const { assert, expect } = require('chai')
 const { addBusinessDays, isBusinessDay } = require('../../app/lib/businessDays')
+const dayjs = require('dayjs')
 
 describe('BusinessDays', () => {
   describe('isBusinessDays', () => {
@@ -29,6 +30,90 @@ describe('BusinessDays', () => {
 
     it('should return `false` if day is sunday', () => {
       assert.isFalse(isBusinessDay(new Date('2020-01-12')))
+    })
+  })
+
+  describe('addBusinessDays', () => {
+    describe('add operation', () => {
+      describe('when adding business days during a week', () => {
+        it('should subtract 4 days', () => {
+          const date = new Date('2020-03-16')
+          expect(dayjs(addBusinessDays(date, '+', 4)).diff(date, 'days'))
+            .to.equal(4)
+        })
+
+        it('should result the right date', () => {
+          expect(addBusinessDays(new Date('2020-03-16'), '+', 4).toISOString())
+            .to.equal('2020-03-20T00:00:00.000Z')
+        })
+      })
+  
+      describe('when adding business days during period with weekend', () => {
+        it('should subtract 7 days', () => {
+          const date = new Date('2020-03-16')
+          expect(dayjs(addBusinessDays(date, '+', 5)).diff(date, 'days'))
+            .to.equal(7)
+        })
+
+        it('should result the right date', () => {
+          expect(addBusinessDays(new Date('2020-03-16'), '+', 5).toISOString())
+            .to.equal('2020-03-23T00:00:00.000Z')
+        })
+      })
+
+      describe('when adding business days during period with more than one weekend', () => {
+        it('should subtract 16 days', () => {
+          const date = new Date('2020-03-16')
+          expect(dayjs(addBusinessDays(date, '+', 12)).diff(date, 'days'))
+            .to.equal(16)
+        })
+
+        it('should result the right date', () => {
+          expect(addBusinessDays(new Date('2020-03-16'), '+', 12).toISOString())
+            .to.equal('2020-04-01T00:00:00.000Z')
+        })
+      })
+    })
+
+    describe('subtract operation', () => {
+      describe('when subtracting business days during a week', () => {
+        it('should add 4 days', () => {
+          const date = new Date('2020-03-20')
+          expect(dayjs(addBusinessDays(date, '-', 4)).diff(date, 'days'))
+            .to.equal(-4)
+        })
+
+        it('should result the right date', () => {
+          expect(addBusinessDays(new Date('2020-03-20'), '-', 4).toISOString())
+            .to.equal('2020-03-16T00:00:00.000Z')
+        })
+      })
+  
+      describe('when subtracting business days during period with weekend', () => {
+        it('should add 7 days', () => {
+          const date = new Date('2020-03-20')
+          expect(dayjs(addBusinessDays(date, '-', 5)).diff(date, 'days'))
+            .to.equal(-7)
+        })
+
+        it('should result the right date', () => {
+          expect(addBusinessDays(new Date('2020-03-20'), '-', 5).toISOString())
+            .to.equal('2020-03-13T00:00:00.000Z')
+        })
+      })
+
+      describe('when subtracting business days during period with more than one weekend', () => {
+        it('should add 16 days', () => {
+          const date = new Date('2020-03-20')
+          expect(dayjs(addBusinessDays(date, '-', 12)).diff(date, 'days'))
+            .to.equal(-16)
+        })
+
+        it('should result the right date', () => {
+          expect(addBusinessDays(new Date('2020-03-20'), '-', 12).toISOString())
+            .to.equal('2020-03-04T00:00:00.000Z')
+        })
+      })
     })
   })
 })
